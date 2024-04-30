558943e4982fcd68c3264e15486c904aef94ef26
MohammadNawasrah <nawasrahmohammad2000@gmail.com>
2024-04-30 16:18:38 +0300

    chart

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -37,7 +37,7 @@ class AdminPanelProvider extends PanelProvider
                 Pages\Dashboard::class,
             ])
 
-            // ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\\Filament\\Widgets')
+            ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\\Filament\\Widgets')
             ->widgets([
                 Test::class
             ])
