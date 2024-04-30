9369559a51f573af8634e19776c5c45db3e400d7
MohammadNawasrah <nawasrahmohammad2000@gmail.com>
2024-04-30 11:57:36 +0300

    table tabs ListPosts.php

--- app/Filament/Resources/PostResource/Pages/ListPosts.php
+++ app/Filament/Resources/PostResource/Pages/ListPosts.php
@@ -5,6 +5,8 @@ namespace App\Filament\Resources\PostResource\Pages;
 use App\Filament\Resources\PostResource;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
+use Filament\Resources\Pages\ListRecords\Tab;
+use Illuminate\Database\Eloquent\Builder;
 
 class ListPosts extends ListRecords
 {
@@ -14,6 +16,17 @@ class ListPosts extends ListRecords
     {
         return [
             Actions\CreateAction::make(),
+        ];
+    }
+    public function getTabs() :array{
+        return [
+            "All"=>Tab::make(),
+            "Published"=>Tab::make()->modifyQueryUsing(function (Builder $query){
+                return $query->where("published",true);
+            }),
+            "Un published"=>Tab::make()->modifyQueryUsing(function (Builder $query){
+                return $query->where("published",false);
+            }),
         ];
     }
 }
